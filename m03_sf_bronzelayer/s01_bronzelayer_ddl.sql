6f0f711bf70e5d947f3ec1cd5d2cead27944968b
karpagarajkumar <139135626+karpagarajkumar@users.noreply.github.com>
2023-07-13 12:14:25 +0100

    Update s01_bronzelayer_ddl.sql

--- m03_sf_bronzelayer/s01_bronzelayer_ddl.sql
+++ m03_sf_bronzelayer/s01_bronzelayer_ddl.sql
@@ -1,70 +1,70 @@
 --ddl statement for bronze table
 create or replace table project_crime.bronze_layer.asc_street_raw_br
 (
-	crime_id 				string,	
-	month 					string,	
-	reported_by				string,
-	falls_within 			string,	
-	longitude 				string,	
-	latitude 				string,	
-	location 				string,	
-	lsoa_code 				string,	
-	laoa_name 				string,	
-	crime_type 				string,	
-	last_outcome_category 	string,	
-	context 				string,
+	crime_id 				varchar,	
+	month 					varchar,	
+	reported_by				varchar,
+	falls_within 			varchar,	
+	longitude 				varchar,	
+	latitude 				varchar,	
+	location 				varchar,	
+	lsoa_code 				varchar,	
+	laoa_name 				varchar,	
+	crime_type 				varchar,	
+	last_outcome_category 	varchar,	
+	context 				varchar,
 	merge_flag 				boolean
 )
 ;
 
 create or replace table project_crime.bronze_layer.sy_street_raw_br
 (
-	crime_id 				string,	
-	month 					string,	
-	reported_by				string,
-	falls_within 			string,	
-	longitude 				string,	
-	latitude 				string,	
-	location 				string,	
-	lsoa_code 				string,	
-	laoa_name 				string,	
-	crime_type 				string,	
-	last_outcome_category 	string,	
-	context 				string,
+	crime_id 				varchar,	
+	month 					varchar,	
+	reported_by				varchar,
+	falls_within 			varchar,	
+	longitude 				varchar,	
+	latitude 				varchar,	
+	location 				varchar,	
+	lsoa_code 				varchar,	
+	laoa_name 				varchar,	
+	crime_type 				varchar,	
+	last_outcome_category 	varchar,	
+	context 				varchar,
 	merge_flag 				boolean
 )
 ;
 
 create or replace table project_crime.bronze_layer.asc_street_br
 (
-	crime_id 				string,	
-	month 					string,	
-	reported_by				string,
-	falls_within 			string,	
-	longitude 				string,	
-	latitude 				string,	
-	location 				string,	
-	lsoa_code 				string,	
-	laoa_name 				string,	
-	crime_type 				string,	
-	last_outcome_category 	string,	
+	crime_id 				varchar,	
+	month 					varchar,	
+	reported_by				varchar,
+	falls_within 			varchar,	
+	longitude 				varchar,	
+	latitude 				varchar,	
+	location 				varchar,	
+	lsoa_code 				varchar,	
+	laoa_name 				varchar,	
+	crime_type 				varchar,	
+	last_outcome_category 	varchar,	
 	proc_date 				date
 )
 ;
 
 create or replace table project_crime.bronze_layer.sy_street_br
 (
-	crime_id 				string,	
-	month 					string,	
-	reported_by				string,
-	falls_within 			string,	
-	longitude 				string,	
-	latitude 				string,	
-	location 				string,	
-	lsoa_code 				string,	
-	laoa_name 				string,	
-	crime_type 				string,	
-	last_outcome_category 	string,	
+	crime_id 				varchar,	
+	month 					varchar,	
+	reported_by				varchar,
+	falls_within 			varchar,	
+	longitude 				varchar,	
+	latitude 				varchar,	
+	location 				varchar,	
+	lsoa_code 				varchar,	
+	laoa_name 				varchar,	
+	crime_type 				varchar,	
+	last_outcome_category 	varchar,	
 	proc_date 				date
 )
 ;
