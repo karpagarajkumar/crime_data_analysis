f2b97fbfa6cf744282037befbc611feb6c2f05c4
karpagarajkumar <139135626+karpagarajkumar@users.noreply.github.com>
2023-07-13 12:10:15 +0100

    Update s01_goldlayer_ddl.sql

--- m05_sf_goldlayer/s01_goldlayer_ddl.sql
+++ m05_sf_goldlayer/s01_goldlayer_ddl.sql
@@ -9,20 +9,21 @@ create or replace table project_crime.gold_layer.area_dim
 
 create or replace table project_crime.gold_layer.lsoa_dim
 (
-	lsoa_id 	string,
-	lsoa_name 	string
+	lsoa_id 	Varchar,
+	lsoa_name 	Varchar
 )
 ;
 
 create table project_crime.gold_layer.crime_data_fact_tbl
 (
-	crime_id 				string,
-	month 					string,
-	reported_by 			string,
-	location 				string,
-	lsoa_code 				string,
-	crime_type 				string,
-	last_outcome_category 	string
-) 
-partitioned by (area_id string,event_date date)
-;
+	crime_id 				Varchar,
+	month 					Varchar,
+	area_id 				Varchar
+	reported_by 				Varchar,
+	location 				Varchar,
+	lsoa_code 				Varchar,
+	crime_type 				Varchar,
+	last_outcome_category 			Varchar
+	event_date date
+	cluster by (area_id,event_date)
+);
