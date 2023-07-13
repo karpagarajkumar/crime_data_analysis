43b213aeb6efc3b2a04fdf4ceada52c3c26e586a
karpagarajkumar <139135626+karpagarajkumar@users.noreply.github.com>
2023-07-13 12:12:22 +0100

    Update s01_rawlayer_ddl.sql

--- m02_sf_raw_layer/s01_rawlayer_ddl.sql
+++ m02_sf_raw_layer/s01_rawlayer_ddl.sql
@@ -1,35 +1,35 @@
 --ddl statement for raw table
 create or replace table project_crime.raw_layer.asc_street_raw
 (
-	crime_id 				string,	
-	month 					string,	
-	reported_by				string,
-	falls_within 			string,	
-	longitude 				string,	
-	latitude 				string,	
-	location 				string,	
-	lsoa_code 				string,	
-	laoa_name 				string,	
-	crime_type 				string,	
-	last_outcome_category 	string,	
-	context 				string
+	crime_id 				varchar,	
+	month 					varchar,	
+	reported_by				varchar,
+	falls_within 			varchar,	
+	longitude 				varchar,	
+	latitude 				varchar,	
+	location 				varchar,	
+	lsoa_code 				varchar,	
+	laoa_name 				varchar,	
+	crime_type 				varchar,	
+	last_outcome_category 	varchar,	
+	context 				varchar
 )
 ;
 
 create or replace table project_crime.raw_layer.sy_street_raw
 (
-	crime_id 				string,	
-	month 					string,	
-	reported_by				string,
-	falls_within 			string,	
-	longitude 				string,	
-	latitude 				string,	
-	location 				string,	
-	lsoa_code 				string,	
-	laoa_name 				string,	
-	crime_type 				string,	
-	last_outcome_category 	string,	
-	context 				string
+	crime_id 				varchar,	
+	month 					varchar,	
+	reported_by				varchar,
+	falls_within 			varchar,	
+	longitude 				varchar,	
+	latitude 				varchar,	
+	location 				varchar,	
+	lsoa_code 				varchar,	
+	laoa_name 				varchar,	
+	crime_type 				varchar,	
+	last_outcome_category 	varchar,	
+	context 				varchar
 )
 ;
 
